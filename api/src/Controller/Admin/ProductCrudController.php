f942d036e9b6ea46e9c9e6b1b8d9318300ad44e3
Yoyick <yoyick@gmail.com>
2026-03-28 14:06:49 +0100

    added admin dashboard and crud

---
+++ api/src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name', 'Product Naam'),
+            MoneyField::new('price')->setCurrency('EUR'),
+            // EasyAdmin snapt zelf dat 'price' in de DB een integer/decimal is.
+        ];
+    }
+}
